22804f5fb705536b8f43b149224993632a1b4ca6
Jesse Tremblay <jessetrem@gmail.com>
2023-10-06 19:53:12 -0400

    Added directional pasting in a way that fits the new way tiled handles pastes

--- src/tiled/mapeditor.cpp
+++ src/tiled/mapeditor.cpp
@@ -174,7 +174,10 @@ MapEditor::MapEditor(QObject *parent)
     mBucketFillTool = new BucketFillTool(this);
     mEditPolygonTool = new EditPolygonTool(this);
     mShapeFillTool = new ShapeFillTool(this);
-    CreateObjectTool *tileObjectsTool = new CreateTileObjectTool(this);
+    // EDEN CHANGE
+    mTileObjectTool = new CreateTileObjectTool(this);
+    // EDEN CHANGE END
+    CreateObjectTool *tileObjectsTool = mTileObjectTool;
     CreateTemplateTool *templatesTool = new CreateTemplateTool(this);
     CreateObjectTool *rectangleObjectsTool = new CreateRectangleObjectTool(this);
     CreateObjectTool *pointObjectsTool = new CreatePointObjectTool(this);
@@ -534,6 +537,22 @@ void MapEditor::performStandardAction(StandardAction action)
     case PasteInPlaceAction:
         paste(ClipboardManager::PasteInPlace);
         break;
+
+    //EDEN CHANGES
+    case PasteUpAction:
+        paste(ClipboardManager::PasteUp);
+        break;
+    case PasteDownAction:
+        paste(ClipboardManager::PasteDown);
+        break;
+    case PasteLeftAction:
+        paste(ClipboardManager::PasteLeft);
+        break;
+    case PasteRightAction:
+        paste(ClipboardManager::PasteRight);
+        break;
+    //EDEN CHANGES END
+
     case DeleteAction:
         if (mEditPolygonTool->hasSelectedHandles())
             mEditPolygonTool->deleteNodes();
